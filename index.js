84dd6f24b8327272d4202199ab746a64e1a093b8
足羽川永都 <wakeup@8am.jp>
2020-03-28 18:35:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+/*
+  「福井県オープンデータライブラリ」の「ごみ収集日一覧」ページで公開されている
+  収集日のCSVデータ（Shift-JIS）をJSONに変換します。
+*/
+
 const scraper = require('./lib/scraper');
 const generator = require('./lib/generator');
 
 (async () => {
+    // 「ごみ収集日一覧」ページのURL
     const page = 'https://www.pref.fukui.lg.jp/doc/toukei-jouhou/opendata/list_ct_gomisyusyubi.html'
+    // ページ内の17市町のCSVデータのURLを取得
     const resources = await scraper.scrape(page)
+    // すべてのCSVを取得してJSONに変換して出力
     await Promise.all(resources.map(resource => generator.generate(resource)));
     console.log('exit.')
 })();
