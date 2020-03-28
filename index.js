d7d77fa22d8e2a2bdeb41a21f5765586b6cc6e0e
8amjp <wakeup@8am.jp>
2020-03-28 19:41:34 +0900

    0.0.1

--- index.js
+++ index.js
@@ -3,15 +3,14 @@
   収集日のCSVデータ（Shift-JIS）をJSONに変換します。
 */
 
-const scraper = require('./lib/scraper')
-const generator = require('./lib/generator')
+const scraper = require('./lib/scraper');
+const generator = require('./lib/generator');
+const page = 'https://www.pref.fukui.lg.jp/doc/toukei-jouhou/opendata/list_ct_gomisyusyubi.html'; // 「ごみ収集日一覧」ページのURL
 
 (async () => {
-    // 「ごみ収集日一覧」ページのURL
-    const page = 'https://www.pref.fukui.lg.jp/doc/toukei-jouhou/opendata/list_ct_gomisyusyubi.html'
     // ページ内の17市町のCSVデータのURLを取得
     const resources = await scraper.scrape(page)
     // すべてのCSVを取得してJSONに変換して出力
-    await Promise.all(resources.map(resource => generator.generate(resource)));
-    console.log('exit.')
+    await Promise.all(resources.map(resource => generator.generate(resource)))
+    console.log('できたよ！')
 })();
